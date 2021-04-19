fb9ceb123ef1610c59be65e0eed25d2dbafa3cd0
keshav72727 <75607598+keshav72727@users.noreply.github.com>
2021-04-19 15:22:53 +0530

    Add files via upload

---
+++ player.js
@@ -0,0 +1,54 @@
+class Player {
+    constructor() {
+        this.index = null;
+        this.distance = 0;
+        this.name = null;
+        this.score =0;
+    }
+
+    getCount() {
+        var playerCountRef = database.ref('playerCount');
+        playerCountRef.on("value", (data) => {
+            playerCount = data.val();
+        })
+    }
+        
+
+    
+    getScore (){
+        var getScore = database.ref('Score');
+        getScore.on("value",(data)=>{
+            this.score = data.val();
+        })
+    }
+
+    static updateScore(Score){
+        database.ref('/').update({
+            Score : score
+        })
+    }
+
+    updateCount(count) {
+        database.ref('/').update({
+            playerCount: count
+        });
+    }
+
+    update() {
+        var playerIndex = "players/player" + this.index;
+        database.ref(playerIndex).set({
+            name: this.name,
+            distance: this.distance,
+            score:this.score
+        });
+    }
+
+    static getPlayerInfo() {
+        var playerInfoRef = database.ref('players');
+        playerInfoRef.on("value", (data) => {
+            allPlayers = data.val();
+        })
+    }
+
+    
+}
